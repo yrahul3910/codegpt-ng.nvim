a63ff744e2b5c97681a73a8d9ec46c3cb20b07e7
Rahul Yedida <yrahul3910@gmail.com>
2026-02-13 15:55:02 -0500

    feat: add make_stream_call to anthropic provider

--- lua/codegpt/commands.lua
+++ lua/codegpt/commands.lua
@@ -1,11 +1,11 @@
 ---@module 'plenary.curl'
 
-local Utils = require("codegpt.utils")
-local Ui = require("codegpt.ui")
-local Providers = require("codegpt.providers")
-local Api = require("codegpt.api")
-local Config = require("codegpt.config")
-local models = require("codegpt.models")
+local Utils = require "codegpt.utils"
+local Ui = require "codegpt.ui"
+local Providers = require "codegpt.providers"
+local Api = require "codegpt.api"
+local Config = require "codegpt.config"
+local models = require "codegpt.models"
 
 local M = {}
 
@@ -14,8 +14,8 @@ local M = {}
 ---@param bufnr integer
 ---@param range Range4
 local text_popup_stream = function(job, stream, bufnr, range)
-	local popup_filetype = Config.opts.ui.text_popup_filetype
-	Ui.popup_stream(job, stream, popup_filetype, bufnr, range)
+  local popup_filetype = Config.opts.ui.text_popup_filetype
+  Ui.popup_stream(job, stream, popup_filetype, bufnr, range)
 end
 
 ---@param job Job
@@ -23,44 +23,44 @@ end
 ---@param bufnr integer
 ---@param range Range4
 local function replacement(job, lines, bufnr, range)
-	local start_row, _, end_row, _ = unpack(range)
-	lines = Utils.strip_reasoning(lines, "<think>", "</think>")
-	lines = Utils.trim_to_code_block(lines)
-	lines = Utils.remove_trailing_whitespace(lines)
-	Utils.fix_indentation(bufnr, start_row, end_row, lines)
-	-- if the buffer is not valid, open a popup. This can happen when the user closes the previous popup window before the request is finished.
-	if vim.api.nvim_buf_is_valid(bufnr) ~= true then
-		Ui.popup(job, lines, Utils.get_filetype(bufnr), bufnr, range)
-	else
-		return lines
-	end
+  local start_row, _, end_row, _ = unpack(range)
+  lines = Utils.strip_reasoning(lines, "<think>", "</think>")
+  lines = Utils.trim_to_code_block(lines)
+  lines = Utils.remove_trailing_whitespace(lines)
+  Utils.fix_indentation(bufnr, start_row, end_row, lines)
+  -- if the buffer is not valid, open a popup. This can happen when the user closes the previous popup window before the request is finished.
+  if vim.api.nvim_buf_is_valid(bufnr) ~= true then
+    Ui.popup(job, lines, Utils.get_filetype(bufnr), bufnr, range)
+  else
+    return lines
+  end
 end
 
 M.CallbackTypes = {
-	["text_popup_stream"] = text_popup_stream,
-	["text_popup"] = function(job, lines, bufnr, range)
-		local popup_filetype = Config.opts.ui.text_popup_filetype
-		Ui.popup(job, lines, popup_filetype, bufnr, range)
-	end,
-	["code_popup"] = function(job, lines, bufnr, range)
-		local start_row, _, end_row, _ = unpack(range)
-		lines = Utils.trim_to_code_block(lines)
-		Utils.fix_indentation(bufnr, start_row, end_row, lines)
-		Ui.popup(job, lines, Utils.get_filetype(bufnr), bufnr, range)
-	end,
-	["replace_lines"] = function(job, lines, bufnr, range)
-		lines = replacement(job, lines, bufnr, range)
-		Utils.replace_lines(lines, bufnr, range)
-	end,
-	["insert_lines"] = function(job, lines, bufnr, range)
-		lines = replacement(job, lines, bufnr, range)
-		Utils.insert_lines(lines)
-	end,
-	["prepend_lines"] = function(job, lines, bufnr, range)
-		lines = replacement(job, lines, bufnr, range)
-		Utils.prepend_lines(lines)
-	end,
-	["custom"] = nil,
+  ["text_popup_stream"] = text_popup_stream,
+  ["text_popup"] = function(job, lines, bufnr, range)
+    local popup_filetype = Config.opts.ui.text_popup_filetype
+    Ui.popup(job, lines, popup_filetype, bufnr, range)
+  end,
+  ["code_popup"] = function(job, lines, bufnr, range)
+    local start_row, _, end_row, _ = unpack(range)
+    lines = Utils.trim_to_code_block(lines)
+    Utils.fix_indentation(bufnr, start_row, end_row, lines)
+    Ui.popup(job, lines, Utils.get_filetype(bufnr), bufnr, range)
+  end,
+  ["replace_lines"] = function(job, lines, bufnr, range)
+    lines = replacement(job, lines, bufnr, range)
+    Utils.replace_lines(lines, bufnr, range)
+  end,
+  ["insert_lines"] = function(job, lines, bufnr, range)
+    lines = replacement(job, lines, bufnr, range)
+    Utils.insert_lines(lines)
+  end,
+  ["prepend_lines"] = function(job, lines, bufnr, range)
+    lines = replacement(job, lines, bufnr, range)
+    Utils.prepend_lines(lines)
+  end,
+  ["custom"] = nil,
 }
 
 --- Combines the final command arguments before the api call.
@@ -70,39 +70,39 @@ M.CallbackTypes = {
 ---@return table opts parsed options
 ---@return boolean is_stream streaming enabled
 local function get_cmd_opts(cmd)
-	local opts = Config.opts.commands[cmd]
-	-- print(vim.inspect(opts))
-	local cmd_defaults = Config.opts.global_defaults
-	local is_stream = false
+  local opts = Config.opts.commands[cmd]
+  -- print(vim.inspect(opts))
+  local cmd_defaults = Config.opts.global_defaults
+  local is_stream = false
 
-	local model
-	if opts ~= nil and opts.model then
-		_, model = models.get_model_by_name(opts.model)
-	else
-		_, model = models.get_model()
-	end
+  local model
+  if opts ~= nil and opts.model then
+    _, model = models.get_model_by_name(opts.model)
+  else
+    _, model = models.get_model()
+  end
 
-	---@type codegpt.CommandOpts
-	--- options priority heighest->lowest: cmd options, model options, global
-	opts = vim.tbl_extend("force", cmd_defaults, model or {}, opts or {})
+  ---@type codegpt.CommandOpts
+  --- options priority heighest->lowest: cmd options, model options, global
+  opts = vim.tbl_extend("force", cmd_defaults, model or {}, opts or {})
 
-	if type(opts.callback_type) == "function" then
-		opts.callback = opts.callback_type
-	else
-		if
-			(
-				(Config.opts.ui.stream_output and opts.callback_type == "text_popup")
-				or opts.callback_type == "text_popup_stream"
-			) and (opts.stream_output ~= false and Config.stream_override ~= false)
-		then
-			opts.callback = text_popup_stream
-			is_stream = true
-		else
-			opts.callback = M.CallbackTypes[opts.callback_type]
-		end
-	end
+  if type(opts.callback_type) == "function" then
+    opts.callback = opts.callback_type
+  else
+    if
+      (
+        (Config.opts.ui.stream_output and opts.callback_type == "text_popup")
+        or opts.callback_type == "text_popup_stream"
+      ) and (opts.stream_output ~= false and Config.stream_override ~= false)
+    then
+      opts.callback = text_popup_stream
+      is_stream = true
+    else
+      opts.callback = M.CallbackTypes[opts.callback_type]
+    end
+  end
 
-	return opts, is_stream
+  return opts, is_stream
 end
 
 ---@param command string
@@ -110,43 +110,43 @@ end
 ---@param text_selection string
 ---@param range Range4
 function M.run_cmd(command, command_args, text_selection, range)
-	local provider = Providers.get_provider()
-	local cmd_opts, is_stream = get_cmd_opts(command)
-	if cmd_opts == nil then
-		vim.notify("Command not found: " .. command, vim.log.levels.ERROR, {
-			title = "CodeGPT",
-		})
-		return
-	end
+  local provider = Providers.get_provider()
+  local cmd_opts, is_stream = get_cmd_opts(command)
+  if cmd_opts == nil then
+    vim.notify("Command not found: " .. command, vim.log.levels.ERROR, {
+      title = "CodeGPT",
+    })
+    return
+  end
 
-	local bufnr = vim.api.nvim_get_current_buf()
-	local new_callback = nil
+  local bufnr = vim.api.nvim_get_current_buf()
+  local new_callback = nil
 
-	if is_stream then
-		new_callback = function(stream, job)
-			cmd_opts.callback(job, stream, bufnr, range)
-		end
-	else
-		new_callback = function(lines, job) -- called from Provider.handle_response
-			cmd_opts.callback(job, lines, bufnr, range)
-		end
-	end
+  if is_stream then
+    new_callback = function(stream, job)
+      cmd_opts.callback(job, stream, bufnr, range)
+    end
+  else
+    new_callback = function(lines, job) -- called from Provider.handle_response
+      cmd_opts.callback(job, lines, bufnr, range)
+    end
+  end
 
-	local request = provider.make_request(command, cmd_opts, command_args, text_selection, is_stream)
-	if Config.debug_prompt then
-		print(vim.fn.json_encode(request))
-		return
-	end
-	if is_stream then
-		provider.make_stream_call(request, new_callback)
-	else
-		provider.make_call(request, new_callback)
-	end
+  local request = provider.make_request(command, cmd_opts, command_args, text_selection, is_stream)
+  if Config.debug_prompt then
+    print(vim.fn.json_encode(request))
+    return
+  end
+  if is_stream then
+    provider.make_stream_call(request, new_callback)
+  else
+    provider.make_call(request, new_callback)
+  end
 end
 
 ---@return string
 function M.get_status(...)
-	return Api.get_status(...)
+  return Api.get_status(...)
 end
 
 M.get_cmd_opts = get_cmd_opts
--- lua/codegpt/providers/anthropic.lua
+++ lua/codegpt/providers/anthropic.lua
@@ -1,130 +1,164 @@
 -- TODO: review and update to NG
-local curl = require("plenary.curl")
-local Render = require("codegpt.template_render")
-local Utils = require("codegpt.utils")
-local Api = require("codegpt.api")
-local Config = require("codegpt.config")
-local errors = require("codegpt.errors")
+local curl = require "plenary.curl"
+local Render = require "codegpt.template_render"
+local Utils = require "codegpt.utils"
+local Api = require "codegpt.api"
+local Config = require "codegpt.config"
+local errors = require "codegpt.errors"
 
 local M = {}
 
 local function generate_messages(command, cmd_opts, command_args, text_selection)
-	local user_message = Render.render(command, cmd_opts.user_message_template, command_args, text_selection, cmd_opts)
+  local user_message = Render.render(command, cmd_opts.user_message_template, command_args, text_selection, cmd_opts)
 
-	local messages = {}
-	if user_message ~= nil and user_message ~= "" then
-		table.insert(messages, { role = "user", content = user_message })
-	end
+  local messages = {}
+  if user_message ~= nil and user_message ~= "" then
+    table.insert(messages, { role = "user", content = user_message })
+  end
 
-	return messages
+  return messages
 end
 
-function M.make_request(command, cmd_opts, command_args, text_selection)
-	local system_message =
-		Render.render(command, cmd_opts.system_message_template, command_args, text_selection, cmd_opts, true)
-	local messages = generate_messages(command, cmd_opts, command_args, text_selection)
+---@param command string
+---@param cmd_opts codegpt.CommandOpts
+---@param command_args string
+---@param text_selection string
+---@param is_stream? boolean
+function M.make_request(command, cmd_opts, command_args, text_selection, is_stream)
+  local system_message =
+    Render.render(command, cmd_opts.system_message_template, command_args, text_selection, cmd_opts, true)
+  local messages = generate_messages(command, cmd_opts, command_args, text_selection)
 
-	-- context window is 100k-200k tokens
-	-- but the output is fixed at 4096 tokens
-	local max_tokens = 4096
+  -- context window is 100k-200k tokens
+  -- but the output is fixed at 4096 tokens
+  local max_tokens = 4096
 
-	local request = {
-		temperature = cmd_opts.temperature or 1.0,
-		max_tokens = max_tokens,
-		model = cmd_opts.model,
-		system = system_message,
-		messages = messages,
-	}
+  local request = {
+    temperature = cmd_opts.temperature or 1.0,
+    max_tokens = max_tokens,
+    model = cmd_opts.model,
+    system = system_message,
+    messages = messages,
+    stream = is_stream or false,
+  }
 
-	return request
+  return request
 end
 
 function M.make_headers()
-	local api_key = vim.g["codegpt_anthropic_api_key"] or os.getenv("ANTHROPIC_API_KEY")
+  local api_key = vim.g["codegpt_anthropic_api_key"] or os.getenv "ANTHROPIC_API_KEY"
 
-	if not api_key then
-		error(
-			"Anthropic API Key not found, set in vim with 'codegpt_anthropic_api_key' or as the env variable 'ANTHROPIC_API_KEY'"
-		)
-	end
+  if not api_key then
+    error "Anthropic API Key not found, set in vim with 'codegpt_anthropic_api_key' or as the env variable 'ANTHROPIC_API_KEY'"
+  end
 
-	return {
-		["Content-Type"] = "application/json",
-		["X-API-Key"] = api_key,
-		["anthropic-version"] = "2023-06-01",
-	}
+  return {
+    ["Content-Type"] = "application/json",
+    ["X-API-Key"] = api_key,
+    ["anthropic-version"] = "2023-06-01",
+  }
 end
 
 local function curl_callback(response, cb)
-	local status = response.status
-	local body = response.body
-	if status ~= 200 then
-		body = body:gsub("%s+", " ")
-		print("Error: " .. status .. " " .. body)
-		return
-	end
+  local status = response.status
+  local body = response.body
+  if status ~= 200 then
+    body = body:gsub("%s+", " ")
+    print("Error: " .. status .. " " .. body)
+    return
+  end
 
-	if body == nil or body == "" then
-		print("Error: No body")
-		return
-	end
+  if body == nil or body == "" then
+    print "Error: No body"
+    return
+  end
 
-	vim.schedule_wrap(function(msg)
-		local json = vim.fn.json_decode(msg)
-		M.handle_response(json, cb)
-	end)(body)
+  vim.schedule_wrap(function(msg)
+    local json = vim.fn.json_decode(msg)
+    M.handle_response(json, cb)
+  end)(body)
 
-	Api.run_finished_hook()
+  Api.run_finished_hook()
 end
 
 function M.handle_response(json, cb)
-	if json == nil then
-		print("Response empty")
-	elseif json.error then
-		print("Error: " .. json.error.message)
-	elseif json.stop_reason ~= "end_turn" then
-		print("Response is incomplete. Payload: " .. vim.fn.json_encode(json))
-	else
-		if json.content[1] ~= nil then
-			local response_text = json.content[1].text
+  if json == nil then
+    print "Response empty"
+  elseif json.error then
+    print("Error: " .. json.error.message)
+  elseif json.stop_reason ~= "end_turn" then
+    print("Response is incomplete. Payload: " .. vim.fn.json_encode(json))
+  else
+    if json.content[1] ~= nil then
+      local response_text = json.content[1].text
 
-			if response_text ~= nil then
-				if type(response_text) ~= "string" or response_text == "" then
-					print("Error: No response text " .. type(response_text))
-				else
-					local bufnr = vim.api.nvim_get_current_buf()
-					if Config.opts.clear_visual_selection then
-						vim.api.nvim_buf_set_mark(bufnr, "<", 0, 0, {})
-						vim.api.nvim_buf_set_mark(bufnr, ">", 0, 0, {})
-					end
-					cb(Utils.parse_lines(response_text))
-				end
-			else
-				print("Error: No completion")
-			end
-		else
-			print("Error: No completion")
-		end
-	end
+      if response_text ~= nil then
+        if type(response_text) ~= "string" or response_text == "" then
+          print("Error: No response text " .. type(response_text))
+        else
+          local bufnr = vim.api.nvim_get_current_buf()
+          if Config.opts.clear_visual_selection then
+            vim.api.nvim_buf_set_mark(bufnr, "<", 0, 0, {})
+            vim.api.nvim_buf_set_mark(bufnr, ">", 0, 0, {})
+          end
+          cb(Utils.parse_lines(response_text))
+        end
+      else
+        print "Error: No completion"
+      end
+    else
+      print "Error: No completion"
+    end
+  end
 end
 
 function M.make_call(payload, cb)
-	local payload_str = vim.fn.json_encode(payload)
-	local url = "https://api.anthropic.com/v1/messages"
-	local headers = M.make_headers()
-	Api.run_started_hook()
-	Api.current_job = curl.post(url, {
-		body = payload_str,
-		headers = headers,
-		callback = function(response)
-			curl_callback(response, cb)
-		end,
-		on_error = errors.curl_error,
-	})
+  local payload_str = vim.fn.json_encode(payload)
+  local url = "https://api.anthropic.com/v1/messages"
+  local headers = M.make_headers()
+  Api.run_started_hook()
+  Api.current_job = curl.post(url, {
+    body = payload_str,
+    headers = headers,
+    callback = function(response)
+      curl_callback(response, cb)
+    end,
+    on_error = errors.curl_error,
+  })
+end
+
+---@param payload table payload sent to api
+---@param stream_cb fun(data: table, job: table) callback to handle the resonse json stream
+function M.make_stream_call(payload, stream_cb)
+  local payload_str = vim.fn.json_encode(payload)
+  local url = Config.opts.connection.chat_completions_url
+  local headers = M.make_headers()
+  Api.run_started_hook()
+  Api.current_job = curl.post(url, {
+    body = payload_str,
+    headers = headers,
+    stream = function(error, data, job)
+      if error ~= nil then
+        vim.schedule_wrap(function(err)
+          vim.notify(err, vim.log.levels.ERROR)
+        end)(error)
+      end
+      vim.schedule_wrap(function(dat, jb)
+        stream_cb(dat, jb)
+      end)(data, job)
+    end,
+    callback = function(response)
+      curl_callback(response, nil, true)
+      Api.run_finished_hook()
+    end,
+    on_error = errors.curl_error,
+    insecure = Config.opts.connection.allow_insecure,
+    proxy = Config.opts.connection.proxy,
+  })
 end
 
 function M.get_models()
-	vim.notify("anthropic: not implemented", vim.log.levels.WARN)
+  vim.notify("anthropic: not implemented", vim.log.levels.WARN)
 end
 
 return M
